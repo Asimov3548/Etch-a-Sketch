47d6c6d79107507751d30c5a22f1aa97de5ad187
Pawel_CVO <[166063235+Asimov3548@users.noreply.github.com]>
2024-09-10 14:36:58 +0200

    Clean code, unnecessary comments and functions

--- script.js
+++ script.js
@@ -16,25 +16,17 @@ function makeGrid(numberOfSquares, classToAdd, parentToAppend) {
     }
 }
 
-/* makeGrid(20, "grid_square", ".container"); */
-
 function sketchingOnDiv() {
     const divsToTrack = document.querySelectorAll(".grid_square");
-
+    
     divsToTrack.forEach((div) => {
-        div.addEventListener("mousemove", (e) => {
+        div.addEventListener("mouseover", (e) => {
             let color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`
 
-            /* skrypt zmieniający kolor diva, na który najedzie myszka */
             e.target.style.background = color;
-             
-
-            /* e.target.style.background = 'rgb(255,255,255)'; */ 
         })
     })
 }
-
-
 
 function getGridWidth() {
     const gridWidth = prompt("Set how many squares in a row of grid you want: ");
@@ -44,20 +36,10 @@ function getGridWidth() {
 const buttonSetGridWidth = document.querySelector("button");
 
 buttonSetGridWidth.addEventListener("click", () => {
-    /* getGridWidth(); */
     const drawingArea = document.querySelector(".container");
     drawingArea.innerHTML = "";
 
-
     let gridWidth = prompt("Set how many squares in a row of grid you want: ");
-
-    /* if (gridWidth > 100) {
-        gridWidth = prompt("Set how many squares in a row of grid you want (number can be max 100)");
-    } else {
-        makeGrid(gridWidth, "grid_square", ".container");
-
-        sketchingOnDiv();
-    } */
 
     while (gridWidth > 100) {
         gridWidth = prompt("Set how many squares in a row of grid you want (number can be max 100)");
@@ -66,22 +48,4 @@ buttonSetGridWidth.addEventListener("click", () => {
     makeGrid(gridWidth, "grid_square", ".container");
     
     sketchingOnDiv();
-    
-
-    
 })
-
-/* backup for sketching function */
-/* function sketchingOnDiv() {
-    const divsToTrack = document.querySelectorAll(".grid_square");
-
-    divsToTrack.forEach((div) => {
-        div.addEventListener("mousemove", (e) => {
-            let help = document.querySelector(".support");
-
-            help.textContent = `
-            Client X: ${e.clientX}
-            Client Y: ${e.clientY}`;
-        })
-    })
-} */
